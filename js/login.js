39d7a419799b6438bc3d66a27bad2f24b0022fc1
Wenhaowuuu <wenhaowu92@gmail.com>
2020-04-22 14:26:18 -0700

    0422_login_page

---
+++ js/login.js
@@ -0,0 +1,22 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "culvercity" && password == "opposites"){
+alert ("Login successfully");
+window.location = "https://raimi.maps.arcgis.com/apps/webappviewer/index.html?id=81a425511e2d477e8054bac8f1a88568"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
